c45d6f348a9d2965087bad2441e9b74ab44ff559
Mohammed Refat <mohammedrefat53@outlook.com>
2024-09-30 19:10:23 +0300

    task 4

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', AppController.postNew);
+router.get('/connect', AppController.getConnect);
+router.get('/disconnect', AppController.getDisconnect);
+router.get('/users/me', AppController.getMe);
 
 export default router;
